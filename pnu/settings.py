ac9e0f9bd02a317281d6b24901897abb4c4f5ac9
khj6165 <khj6165@gmail.com>
2020-12-18 12:21:59 +0900

    오류수정

--- pnu/settings.py
+++ pnu/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os, json
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,7 +85,8 @@ DATABASES = {
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
